a90b461119d96b2374550a79b4a5c4bdfefbd1f7
shimasan <shimasanlog.d@gmail.com>
2021-04-21 01:53:41 +0900

    First commit

---
+++ .emacs.d/elpa/company-20210411.2221/company-pkg.el
@@ -0,0 +1,12 @@
+(define-package "company" "20210411.2221" "Modular text completion framework"
+  '((emacs "25.1"))
+  :commit "6eca34e58c37239c3c0e523dd79b4e36c9066dbf" :authors
+  '(("Nikolaj Schumacher"))
+  :maintainer
+  '("Dmitry Gutov" . "dgutov@yandex.ru")
+  :keywords
+  '("abbrev" "convenience" "matching")
+  :url "http://company-mode.github.io/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ .emacs.d/elpa/phi-search-20200510.906/phi-search-pkg.el
@@ -0,0 +1,8 @@
+(define-package "phi-search" "20200510.906" "another incremental search & replace, compatible with \"multiple-cursors\"" 'nil :commit "c34f5800968922d1f9e7b10092b8705d6640ad18" :authors
+  '(("zk_phi"))
+  :maintainer
+  '("zk_phi")
+  :url "http://hins11.yu-yake.com/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
